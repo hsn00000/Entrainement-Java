47b908008b81230bf7a816125d5f5df82f043387
hsn00000 <kanicihasan90@gmail.com>
2025-09-04 11:06:23 +0200

    FEAT : création du constructeur de la classe Souris

--- src/Souris.java
+++ src/Souris.java
@@ -1,10 +1,22 @@
 
 public class Souris {
+    //Constante de la classe
+    public static final int ESPERANCE_VIE_PAR_DEFAUT = 36;
+
     // Attributs
-    public int poids;
-    public int age;
-    public String couleur;
-    public int esperanceVie;
-    public boolean clonee;
+    private int poids;
+    private int age;
+    private String couleur;
+    private int esperanceVie;
+    private boolean clonee;
+
+    // Constructeurs
+    public Souris(int poids, String couleur, int age, int esperanceVie) {
+        this.poids = poids;
+        this.couleur = couleur;
+        this.age = age;
+        this.esperanceVie = esperanceVie;
+        this.clonee = false;
+    }
 
 }
