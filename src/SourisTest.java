605ad5f969ae1ee80364e74089aed174b605a826
hsn00000 <kanicihasan90@gmail.com>
2025-09-04 14:10:36 +0200

    FEAT : Ajout des tests unitaires pour la classe Souris

--- src/SourisTest.java
+++ src/SourisTest.java
@@ -13,10 +13,16 @@ public class SourisTest {
     @Test
     public void testToString() {
 
+        Souris souris = new Souris(100, "blanche", 12, 36);
+        String expected = "Souris de couleur blanche, age : 12, poids : 100kg, esperance de vie : 36 mois.";
+        assertEquals(expected, souris.toString());
     }
 
     @Test
     public void testVieillir() {
-
+        Souris souris = new Souris(100, "blanche", 12, 36);
+        souris.vieillir();
+        assertEquals(13, souris.getAge());
     }
+
 }
